b908e9a02ecd47eaf4285428cec74278553d0b69
bernardocdm <bcdmercado@sga.pucminas.br>
2026-04-10 09:55:12 -0300

    Add section Experiences and fixing strutucture of Contact

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -13,7 +13,7 @@ const contactInfo = [
   {
     icon: Phone,
     label:"Phone",
-
+    value: "+55 31 99983-9934",
   },
   {
     icon: MapPin,
@@ -21,8 +21,6 @@ const contactInfo = [
     value: "Belo Horizonte, MG",
   }
 ]
-
-// where the fuck I find isLoading 
 
 export default function Contact() {
   const [ formData, setFormData ] = useState({
@@ -86,140 +84,138 @@ export default function Contact() {
 
   return (
     <section id="contact">
-      <div className="container mx-auto px-6 relativo z-10">
+      <div className="container mx-auto px-6 relative z-10 ">
         <div className="text-center max-w-3xl mx-auto mb-16">
           <span className="text-white text-sm font-medium tracking-wider uppercase animate-fade-in">Reach me out</span>
-          <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">Way to get <span className="font-serif italic font-normal text-white">in touch with me </span></h2>
+          <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">Ways to get <span className="font-serif italic font-normal text-white">in touch with me </span></h2>
           <p>Something to discuss with me? A project, an idea, or a new opportunity? Feel free to contact me</p>
         </div>
 
-        <div className="grid lg:grid-cols-2 gap-12 max-w-5xl mx-auto">
-          <div className="glass p-8 rounded-3xl border border-primary/30 animate-fade-in animation-dalay-300">
-            <form onSubmit={handleSubmit} action="space-y- "> 
-              <div>
-                <label 
-                  htmlFor="name"
-                  className="mb-2 block text-sm font-medium"
-                >
-                  Name
-                </label>
-                <input 
-                  id="name"
-                  type="text"
-                  placeholder="Name..."
-                  required
-                  value={formData.name}
-                  onChange={(e) => 
-                    setFormData({...formData, name: e.target.value })
-                  }
-                  className="w-full mb-2 px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all"
-                />
-              </div>
-              <div className="">
-                <label 
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium"
-                >
-                    Email
-                </label>
-                <input 
-                  type="text"
-                  placeholder="Email..."
-                  required
-                  value={formData.email}
-                  onChange={(e) =>
-                    setFormData({ ...formData, email: e.target.value })
-                  }
-                  className="w-full mb-2 px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all" 
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="message"
-                  className="mb-2 block text-sm font-medium"
-                >
-                  Message
-                </label>
-                <textarea
-                  rows={5}
-                  type="text"
-                  placeholder="Your message..."
-                  required
-                  value={formData.message}
-                  onChange={(e) =>
-                    setFormData({ ...formData, message: e.target.value })
-                  }
-                  className="w-full mb-2 px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all resize-none"
-                />
-              </div>
-
-              <Button 
-                className="w-full" 
-                size="lg"
-                type="submit"
-                disbled={isLoading}
-              >
-                {isLoading ? (
-                  <>Sending...</>
-                ) : (
-                  <>
-                    Send Message 
-                    <Send className="w-5 h-5"/>
-                  </>
-                )}
-              </Button>
-
-              {submitStatus.type && (
-                <div className={`flex items-center gap-3 p-4 rounded-full ${
-                  submitStatus.type === "sucess"
-                    ? "bg-green-500/10 border border-green-500/20 text-gray-400"
-                    : "bg-red-500/10 border border-red-500/20 text-red-400"
-                } `}
-                >
-                  {submitStatus.type === "sucess" ? (
-                    <CheckCircle className="w-5 h-5 shrink-0" />
-                  ) : (
-                    <AlertCircle className="w-5 h-5 shrink-0"/>
-                  )} 
-                  <p className="text-sm">{submitStatus.message}</p>
-                  
+        <div className="grid lg:grid-cols-2 gap-12 items-stretch">
+          <div className="h-full animate-fade-in animation-delay-400 *:">
+            <div className="glass p-8 rounded-3xl border border-primary/30 animate-fade-in animation-dalay-300">
+              <form onSubmit={handleSubmit} action="space-y- "> 
+                <div>
+                  <label 
+                    htmlFor="name"
+                    className="mb-2 block text-sm font-medium"
+                  >
+                    Name
+                  </label>
+                  <input 
+                    id="name"
+                    type="text"
+                    placeholder="Name..."
+                    required
+                    value={formData.name}
+                    onChange={(e) => 
+                      setFormData({...formData, name: e.target.value })
+                    }
+                    className="w-full mb-2 px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all"
+                  />
                 </div>
-              )}
-            </form>
+                <div className="">
+                  <label 
+                    htmlFor="email"
+                    className="mb-2 block text-sm font-medium"
+                  >
+                      Email
+                  </label>
+                  <input 
+                    type="text"
+                    placeholder="Email..."
+                    required
+                    value={formData.email}
+                    onChange={(e) =>
+                      setFormData({ ...formData, email: e.target.value })
+                    }
+                    className="w-full mb-2 px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all" 
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="message"
+                    className="mb-2 block text-sm font-medium"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    rows={5}
+                    type="text"
+                    placeholder="Your message..."
+                    required
+                    value={formData.message}
+                    onChange={(e) =>
+                      setFormData({ ...formData, message: e.target.value })
+                    }
+                    className="w-full mb-2 px-4 py-3 bg-surface rounded-xl border border-border focus:border-primary focus:ring-1 focus:ring-primary outline-none transition-all resize-none"
+                  />
+                </div>
+
+                <Button 
+                  className="w-full" 
+                  size="lg"
+                  type="submit"
+                  disbled={isLoading}
+                >
+                  {isLoading ? (
+                    <>Sending...</>
+                  ) : (
+                    <>
+                      Send Message 
+                      <Send className="w-5 h-5"/>
+                    </>
+                  )}
+                </Button>
+
+                {submitStatus.type && (
+                  <div className={`flex items-center gap-3 p-4 rounded-full ${
+                    submitStatus.type === "sucess"
+                      ? "bg-green-500/10 border border-green-500/20 text-gray-400"
+                      : "bg-red-500/10 border border-red-500/20 text-red-400"
+                  } `}
+                  >
+                    {submitStatus.type === "sucess" ? (
+                      <CheckCircle className="w-5 h-5 shrink-0" />
+                    ) : (
+                      <AlertCircle className="w-5 h-5 shrink-0"/>
+                    )} 
+                    <p className="text-sm">{submitStatus.message}</p>
+                    
+                  </div>
+                )}
+              </form>
 
             
+            </div>
           </div>
-        </div>
 
-      </div>
-
-      {/* Contact Info */}
-          <div className="space-y-6 animate-fade-in animation-delay-400">
-            <div className="glass rounded-3xl p-8">
-              <h3 className="text-xl font-semibold mb-6">
-                Contact Information
-              </h3>
-              <div className="space-y-4">
+               {/* Contact Info */}
+            <div className="glass rounded-3xl flex items-center justify-center  animate-fade-in animation-delay-400">
+              <div className="space-y-14 ">
                 {contactInfo.map((item, i) => (
                   <a
                     key={i}
                     href={item.href}
-                    className="flex items-center gap-4 p-4 rounded-xl hover:bg-surface transition-colors group"
+                    className="flex items-center gap-4 p-8 rounded-xl hover:bg-surface transition-colors group"
                   >
-                    <div className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
-                      <item.icon className="w-5 h-5 text-primary" />
+                  <div className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                    <item.icon className="w-5 h-5 text-primary" />
+                  </div>
+                  <div>
+                    <div className="text-sm text-muted-foreground">
+                      {item.label}
                     </div>
-                    <div>
-                      <div className="text-sm text-muted-foreground">
-                        {item.label}
-                      </div>
-                      <div className="font-medium">{item.value}</div>
-                    </div>
+                    <div className="font-medium">{item.value}</div>
+                  </div>
                   </a>
                 ))}
               </div>
             </div>
           </div>
+
+
+      </div>
     </section>
   );
 }
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,3 +1,44 @@
 export default function Experience() {
-  return <section></section>;
+  
+  const experiences = [
+    {
+      local:"Controladoria Geral do Estado",
+      date:"10/04/2025 - 10/04/2026",
+      description:"Ipsum dolor kldfldskfnldsnf lkdfnslkfnlksnflk lkdfnlksnflksnflsknf klsndflkksndflsnflk dlkfnlsnflksnfkl dlknflsdkn",
+    },
+    {
+      local:"Controlado Geral do Estado",
+      date:"10/04/2026 - Momento ",
+      description:"Ipsum dolor llnflksdnflndlsfns fnflsdnflsdnflksd fdnflsdknflksnf fklnflsdknflksdnf fklnslkfnslnflsdnf fsdklnflsknd",
+    }
+  ];
+  
+  return (
+    <section id="experience">
+      <div className="flex flex-col items-center">
+        <span className=" text-sm uppercase"> My experiences</span>
+        <div className="p-4 w-full max-w-4xl">
+          <h2 className="p-8 text-5xl text-center text-primary font-bold">Where <span className="italic font-serif text-white font-normal">I've passed</span></h2>
+          <div className="glass rounded-2xl animate-fade-in animation-delay-400 p-6">
+            <div className="space-y-14">
+              {experiences.map((item, i) => (
+                <a 
+                  key={i}
+                  href={item.href}
+                  className="block w-full rounded-xl hover:bg-surface transition-colors group"
+                >
+                  <div className="grid  w-full lg:grid-cols-[140px_160px_1fr] gap-4 h-12 rounded-xl p-4">
+                    <div className="font-medium">{item.date}</div> 
+                    <div className="font-medium ">{item.local}</div> 
+                    <div className="font-medium ">{item.description}</div> 
+                  </div>
+                </a>
+              ))}
+            </div> 
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
